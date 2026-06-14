0903c51275b18c38e732f07f5ea758c0e647699c
savargb <savargb123@gmail.com>
2026-06-14 12:58:46 +0545

    v3.

---
+++ countrycode.py
@@ -0,0 +1,10 @@
+countrycode = {
+    "India": "091",
+    "australia": "061",
+    "nepal": "977",
+}
+print("country code for india is - ")
+print(countrycode.get('India', "Not found"))
+
+print("Country code for japan is - ")
+print(countrycode.get('japan', "Not found"))
